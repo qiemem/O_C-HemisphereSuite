febccfeb63d99791d801561dd8bdcf139edd0aff
Bryan Head <bryan.head@gmail.com>
2022-01-11 10:33:19 -0800

    Add low res saving of drummap params

--- software/o_c_REV/HEM_DrumMap.ino
+++ software/o_c_REV/HEM_DrumMap.ino
@@ -60,7 +60,7 @@ public:
           _fill[0] = constrain(_fill[0]+cv1, 0, 255);
           _chaos = constrain(_chaos+cv2, 0, 255);
         }
-        
+
 
         if (Clock(1)) Reset(); // Reset
 
@@ -119,7 +119,7 @@ public:
         if (OC::CORE::ticks - last_clock > HEM_DRUMMAP_AUTO_RESET_TICKS && step != 0) {
             Reset();
         }
-        
+
     }
 
     void View() {
@@ -169,21 +169,29 @@ public:
           value_animation = HEM_DRUMMAP_VALUE_ANIMATION_TICKS;
         }
     }
-        
+
     uint32_t OnDataRequest() {
         uint32_t data = 0;
-        Pack(data, PackLocation {0,8}, fill[0]); 
-        Pack(data, PackLocation {8,8}, fill[1]); 
-        Pack(data, PackLocation {16,8}, x); 
-        Pack(data, PackLocation {24,8}, y); 
+        Pack(data, PackLocation {0,5}, fill[0] >> 3);
+        Pack(data, PackLocation {5,5}, fill[1] >> 3);
+        Pack(data, PackLocation {10,5}, x >> 3);
+        Pack(data, PackLocation {15,5}, y >> 3);
+        Pack(data, PackLocation {20,5}, chaos >> 3);
+        Pack(data, PackLocation {25, 2}, mode[0]);
+        Pack(data, PackLocation {27, 2}, mode[1]);
+        Pack(data, PackLocation {29, 2}, cv_mode);
         return data;
     }
 
     void OnDataReceive(uint32_t data) {
-        fill[0] = Unpack(data, PackLocation {0,8});
-        fill[1] = Unpack(data, PackLocation {8,8});
-        x = Unpack(data, PackLocation {16,8});
-        y = Unpack(data, PackLocation {24,8});
+        fill[0] = Unpack(data, PackLocation {0,5}) << 3;
+        fill[1] = Unpack(data, PackLocation {5,5}) << 3;
+        x = Unpack(data, PackLocation {10,5}) << 3;
+        y = Unpack(data, PackLocation {15,5}) << 3;
+        chaos = Unpack(data, PackLocation {20,5}) << 3;
+        mode[0] = Unpack(data, PackLocation {25,2});
+        mode[1] = Unpack(data, PackLocation {27,2});
+        cv_mode = Unpack(data, PackLocation {29,2});
     }
 
 protected:
@@ -195,7 +203,7 @@ protected:
         help[HEMISPHERE_HELP_ENCODER]  = "Params/Config";
         //                               "------------------" <-- Size Guide
     }
-    
+
 private:
     const uint8_t *MODE_ICONS[3] = {BD_ICON,SN_ICON,HH_ICON};
     const char *CV_MODE_NAMES[3] = {"FILL A/B", "X/Y", "FA/CHAOS"};
@@ -206,10 +214,10 @@ private:
     int value_animation = 0;
     int knob_accel = 256;
     uint32_t last_clock;
-    
+
     // settings
     int8_t mode[2] = {0, 1};
-    int fill[2] = {128, 128}; 
+    int fill[2] = {128, 128};
     int x = 0;
     int y = 0;
     int chaos = 0;
@@ -232,12 +240,12 @@ private:
       uint8_t quad_x = x << 2;
       uint8_t quad_y = y << 2;
       // return U8Mix(U8Mix(a, b, x << 2), U8Mix(c, d, x << 2), y << 2);
-      // U8Mix returns b * x + a * (255 - x) >> 8 
+      // U8Mix returns b * x + a * (255 - x) >> 8
       uint8_t ab_fade = (b * quad_x + a * (255 - quad_x)) >> 8;
       uint8_t cd_fade = (d * quad_x + c * (255 - quad_x)) >> 8;
       return (cd_fade * quad_y + ab_fade * (255 - quad_y)) >> 8;
     }
-    
+
     void DrawInterface() {
         // output selection
         gfxPrint(1,15,"A:");
@@ -272,7 +280,7 @@ private:
             if (cv_mode == 0) fb = constrain(fb+cv2, 0, 255);
             DrawKnobAt(40,25,20,fb,cursor == 3);
         }
-        
+
         // x & y
         int _x = x;
         if (cv_mode == 1) _x = constrain(_x+cv1, 0, 255);
@@ -282,13 +290,13 @@ private:
         if (cv_mode == 1) _y = constrain(_y+cv2, 0, 255);
         gfxPrint(32,35,"Y");
         DrawKnobAt(40,35,20,_y,cursor == 5);
-        
+
         // chaos
         int _chaos = chaos;
         if (cv_mode == 2) _chaos = constrain(_chaos+cv2, 0, 255);
         gfxPrint(1,45,"CHAOS");
         DrawKnobAt(32,45,28,_chaos,cursor == 6);
-        
+
         // cv input assignment
         gfxIcon(1,57,CV_ICON);
         gfxPrint(10,55,CV_MODE_NAMES[cv_mode]);
@@ -300,7 +308,7 @@ private:
         if (cursor == 0) gfxCursor(14,23,16); // Part A
         if (cursor == 1) gfxCursor(45,23,16); // Part B
         if (cursor == 7) gfxCursor(10,63,50); // CV Assign
-        
+
         // display value for knobs
         if (value_animation > 0 && cursor >= 2 && cursor <= 6) {
           gfxRect(1, 54, 60, 10);
