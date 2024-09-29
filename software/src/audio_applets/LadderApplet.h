d6cc1276a465569e9eb550fdecd24c8e47e0c07d
Bryan Head <bryan.head@gmail.com>
2024-09-29 05:52:01 -0700

    AudioApplets: Hookup audio streams for ladder filter

--- software/src/audio_applets/LadderApplet.h
+++ software/src/audio_applets/LadderApplet.h
@@ -1,20 +1,34 @@
 #include "HemisphereAudioApplet.h"
+#include "AudioPassthrough.h"
+#include <Audio.h>
 
 template <AudioChannels Channels>
 class LadderApplet : public HemisphereAudioApplet {
+
 public:
   const char* applet_name() {
     return "LadderLPF";
   }
 
-  void Start() {}
+  void Start() {
+    for (int i=0; i < Channels; i++) {
+      in_conns[i].connect(input, i, filters[i], 0);
+      out_conns[i].connect(filters[i], 0, output, i);
+    }
+  }
 
-  void Controller() {}
+  void Controller() {
+    for (int i=0; i<Channels; i++) {
+      filters[i].frequency(pitch);
+      filters[i].resonance(0.01f * res);
+    }
+
+  }
 
   void View() {
     const int label_x = 1;
     const int param_x = 38;
-    gfxPrint(label_x, 15, "Pitch:");
+    gfxPrint(label_x, 15, "Freq:");
     gfxStartCursor(param_x, 15);
     graphics.printf("%4d", pitch);
     gfxEndCursor(cursor == 0);
@@ -35,7 +49,7 @@ public:
         pitch = constrain(pitch + direction, 0, 0xffff);
         break;
       case 1:
-        res = constrain(res + direction, 0, 100);
+        res = constrain(res + direction, 0, 180);
         break;
     }
   }
@@ -46,10 +60,10 @@ public:
   void OnDataReceive(uint64_t data) {}
 
   AudioStream* InputStream() override {
-    return nullptr;
+    return &input;
   }
   AudioStream* OutputStream() override {
-    return nullptr;
+    return &output;
   }
   AudioChannels NumChannels() override {
     return Channels;
@@ -60,6 +74,13 @@ protected:
 
 private:
   int cursor = 0;
-  int pitch = 12 * 128;
+  int pitch = 440;
   int res = 0;
+
+  AudioPassthrough<Channels> input;
+  std::array<AudioFilterLadder, Channels> filters;
+  AudioPassthrough<Channels> output;
+
+  std::array<AudioConnection, Channels> in_conns;
+  std::array<AudioConnection, Channels> out_conns;
 };
--- software/src/hemisphere_audio_config.h
+++ software/src/hemisphere_audio_config.h
@@ -12,12 +12,12 @@ const size_t NUM_SLOTS = 5;
 
 std::tuple<InputApplet<0>, InputApplet<1>, OscApplet> mono_input_pool[2];
 std::tuple<InputApplet<0>, InputApplet<1>> stereo_input_pool;
-std::tuple<PassthruApplet<MONO>, DelayApplet, VcaApplet<MONO>>
+std::tuple<PassthruApplet<MONO>, DelayApplet, LadderApplet<MONO>, VcaApplet<MONO>>
   mono_processors_pool[2][NUM_SLOTS - 1];
-std::tuple<PassthruApplet<STEREO>, VcaApplet<STEREO>>
+std::tuple<PassthruApplet<STEREO>, LadderApplet<STEREO>, VcaApplet<STEREO>>
   stereo_processors_pool[NUM_SLOTS - 1];
 
-AudioAppletSubapp<NUM_SLOTS, 3, 2, 3, 2> audio_app(
+AudioAppletSubapp<NUM_SLOTS, 3, 2, 4, 3> audio_app(
   mono_input_pool,
   stereo_input_pool,
   mono_processors_pool,
