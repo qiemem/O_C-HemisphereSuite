c5fff2a2d307b30bdc6bc951bff36db33320f2b1
Bryan Head <bryan.head@gmail.com>
2024-10-11 15:47:39 -0700

    AudioApplets: Simplify param mapping; add default values

--- software/src/HemisphereAudioApplet.h
+++ software/src/HemisphereAudioApplet.h
@@ -3,28 +3,18 @@
 #include "HemisphereApplet.h"
 #include <AudioStream.h>
 
-struct ParamInput {
-  char const* name;
+struct CVInput{
   int8_t source = 0;
 
-  virtual int8_t NumInputs() const = 0;
-  virtual char const* InputName() const = 0;
-
-  void ChangeSource(int direction) {
-    source = constrain(source + direction, 0, NumInputs() - 1);
-  }
-};
-
-struct CVInput : public ParamInput {
-  CVInput(char const* n) {
-    name = n;
-  }
-
-  int In() {
-    if (!source) return 0;
+  int In(int default_value = 0) {
+    if (!source) return default_value;
     return source <= ADC_CHANNEL_LAST
       ? frame.inputs[source - 1]
       : frame.outputs[source - 1 - ADC_CHANNEL_LAST];
+  }
+
+  void ChangeSource(int dir) {
+    source = constrain(source + dir, 0, NumInputs() - 1);
   }
 
   int8_t NumInputs() const {
@@ -41,7 +31,6 @@ struct CVInput : public ParamInput {
 };
 
 struct TrigInput {
-  char const* name;
   uint8_t source = 0;
 };
 
@@ -55,46 +44,5 @@ class HemisphereAudioApplet : public HemisphereApplet {
 public:
   virtual AudioStream* InputStream() = 0;
   virtual AudioStream* OutputStream() = 0;
-
-  virtual inline size_t NumInputParams() const { return 0; }
-  virtual ParamInput* GetInputParam(size_t i) { return nullptr; }
-
-  void DisplayInputMap(ParamInput& input, int index, int cursor) {
-    int y = 10 * index + 15;
-    gfxPrint(1, y, input.name);
-    int px = 63 - 6 * 3;
-    gfxPrint(px, y, input.InputName());
-    if (cursor == index) {
-      gfxCursor(px, y + 9, 6 * 3);
-    }
-  }
-
-  // void ViewInputMappings() {
-  //   for (size_t i = 0; i < NumInputParams(); i++) {
-  //     auto p = GetInputParam(i);
-  //     int y = 10 * i + 15;
-  //     gfxPrint(1, y, p->name);
-  //     int px = 63 - 6 * 3;
-  //     gfxPrint(px, y, p->InputName());
-  //     if (mapping_cursor == static_cast<int>(i)) {
-  //       gfxCursor(px, y + 9, 6 * 3);
-  //     }
-  //   }
-  // };
-  //
-private:
-  int8_t mapping_cursor = 0;
 };
 
-// template <AudioChannels NumIn, AudioChannels NumOut>
-// class HemisphereAudioApplet : public HemisphereApplet {
-// public:
-//   AudioStream &InputStream() override { return input_stream; }
-//   AudioStream &OutputStream() override { return output_stream; }
-//   inline AudioChannels NumInputs() { return NumIn; }
-//   inline AudioChannels NumOutputs() { return NumOut; }
-//
-// protected:
-//   AudioPassthrough<NumIn> input_stream;
-//   AudioPassthrough<NumOut> output_stream;
-// };
--- software/src/audio_applets/VCAApplet.h
+++ software/src/audio_applets/VCAApplet.h
@@ -18,7 +18,8 @@ public:
 
   void Controller() {
     float gain = 0.01f
-      * (level * static_cast<float>(level_cv.In()) / HEMISPHERE_MAX_INPUT_CV
+      * (level * static_cast<float>(level_cv.In(HEMISPHERE_MAX_INPUT_CV))
+           / HEMISPHERE_MAX_INPUT_CV
          + offset);
     for (int i = 0; i < Channels; i++) {
       amps[i].gain(gain);
@@ -46,43 +47,11 @@ public:
     gfxStartCursor();
     gfxPrintIcon(PARAM_MAP_ICONS + 8 * shape_cv.source);
     gfxEndCursor(cursor == 4);
-    // graphics.printf("%4d%%", shape_cv);
-    //
-    //
-    //
-    //
-    // // if (cursor >= NUM_PARAMS) {
-    // //   ViewInputMappings();
-    // //   return;
-    // // }
-    //
-    // const int label_x = 1;
-    // const int param_x = 38;
-    // gfxPrint(label_x, 15, "Level:");
-    // gfxStartCursor(param_x, 15);
-    // graphics.printf("%3d%%", level);
-    // gfxEndCursor(cursor == 0);
-    //
-    // gfxPrint(label_x, 25, "Bias:");
-    // gfxStartCursor(param_x, 25);
-    // graphics.printf("%3d%%", offset);
-    // gfxEndCursor(cursor == 1);
-    //
-    // gfxPrint(label_x, 35, "Shape:");
-    // gfxStartCursor(param_x, 35);
-    // graphics.printf("%3d%%", shape);
-    // gfxEndCursor(cursor == 2);
-  }
-
-  void ViewInputMappings() {
-    int c = cursor - NUM_PARAMS;
-    DisplayInputMap(level_cv, 0, c);
-    DisplayInputMap(shape_cv, 1, c);
   }
 
   void OnEncoderMove(int direction) {
     if (!EditMode()) {
-      MoveCursor(cursor, direction, NUM_PARAMS + NumInputParams() - 1);
+      MoveCursor(cursor, direction, NUM_PARAMS - 1);
       return;
     }
     switch (cursor) {
@@ -116,26 +85,17 @@ public:
     return &output;
   }
 
-  inline size_t NumInputParams() const {
-    return 2;
-  }
-
-  ParamInput* GetInputParam(size_t i) {
-    ParamInput* params[] = {&level_cv, &shape_cv};
-    return params[i];
-  }
-
 protected:
   void SetHelp() override {}
 
 private:
-  const int NUM_PARAMS = 3;
+  const int NUM_PARAMS = 5;
   int cursor = 0;
   int level = 100;
-  CVInput level_cv{"Level"};
+  CVInput level_cv;
   int offset = 0;
   int shape = 0;
-  CVInput shape_cv{"Shape"};
+  CVInput shape_cv;
 
   AudioPassthrough<Channels> input;
   std::array<AudioAmplifier, Channels> amps;
